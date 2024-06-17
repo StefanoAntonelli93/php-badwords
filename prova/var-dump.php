b5f0123917434ad990d4512b0e0f37877ecd1699
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-06-18 00:01:29 +0200

    [FIX] modified gitignore

--- prova/var-dump.php
+++
@@ -1,9 +0,0 @@
-<?php
-// meglio snake case in php
-
-$prova_array_num = [1,2,3];
-$prova_array_string = ['ciao','arrivederci','addio'];
-
-// simile a console.log
-var_dump($prova_array_num);
-var_dump($prova_array_string);
